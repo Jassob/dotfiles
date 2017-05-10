6df8a3a60b92405d6663757e627e72e96e5d1afb
Jacob Jonsson <jassob@live.se>
2017-05-10 23:16:47 +0200

    Added muted information

--- xmonad/xmobar-volume.sh
+++ xmonad/xmobar-volume.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
-amixer get Master | egrep -o "[0-9]+%" | uniq | while read line ; do
-   echo $line
-done
+# Extract amixer Master information
+LINE=$(amixer get Master | grep -E "[0-9]+%")
+VOL=$(echo $LINE | egrep -o "[0-9]+%" | uniq)
+
+# Add the muted line if the Master device is off (muted)
+if [[ $LINE =~ "off" ]] ; then
+    echo "muted (${VOL})"
+else
+    echo ${VOL}
+fi
