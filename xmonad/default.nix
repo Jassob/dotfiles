bca0fb3188c747e5624660065057529c5c6333c7
Jacob Jonsson <jassob@live.se>
2017-08-29 14:32:16 +0200

    Made my xmonad config use nix instead of stack

---
+++ xmonad/default.nix
@@ -0,0 +1,25 @@
+{ mkDerivation, array, base, bytestring, containers, data-default
+, data-default-class, data-default-instances-containers
+, data-default-instances-dlist, data-default-instances-old-locale
+, deepseq, directory, dlist, extensible-exceptions, filepath
+, ghc-prim, integer-gmp, mtl, old-locale, old-time, process, random
+, rts, setlocale, stdenv, time, transformers, unix, utf8-string
+, X11, X11-xft, xmonad, xmonad-contrib
+}:
+mkDerivation {
+  pname = "xmonad-config";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    array base bytestring containers data-default data-default-class
+    data-default-instances-containers data-default-instances-dlist
+    data-default-instances-old-locale deepseq directory dlist
+    extensible-exceptions filepath ghc-prim integer-gmp mtl old-locale
+    old-time process random rts setlocale time transformers unix
+    utf8-string X11 X11-xft xmonad xmonad-contrib
+  ];
+  jailbreak = true;
+  license = stdenv.lib.licenses.bsd3;
+}
