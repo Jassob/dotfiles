6e164d1ac8ab0d858352f25fe41f909335d3a55f
Jacob Jonsson <jassob@live.se>
2017-09-10 01:40:12 +0200

    Removed stuff that I do not use.
    Also structured the file a bit.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,38 +1,54 @@
+{- XMonad core
+--------------------------------------------------}
 import XMonad
 import qualified XMonad.StackSet as W
-import XMonad.Actions.CopyWindow
-import XMonad.Actions.Search
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.LayoutHints
-import XMonad.Layout.Mosaic
-import XMonad.Layout.NoBorders
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.Spacing
-import XMonad.Layout.Tabbed
-import XMonad.Layout.ToggleLayouts
-import XMonad.Prompt
 
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Scratchpad
-import System.IO
-import Graphics.X11.ExtraTypes.XF86
+{- Actions
+--------------------------------------------------}
+import XMonad.Actions.CopyWindow (copyToAll, killAllOtherCopies)
+import XMonad.Actions.Search (promptSearch, duckduckgo)
 
--- NOTES: 0.10 works much better than 0.9, unfortunately distros mostly package 0.9 atm
--- xmobar and fullscreen flash vids (youtube): http://code.google.com/p/xmobar/issues/detail?id=41
+{- Hooks
+--------------------------------------------------}
+import XMonad.Hooks.DynamicLog (PP(..), dynamicLogWithPP, xmobarColor, wrap)
+import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook
+                                , docksStartupHook, manageDocks)
+import XMonad.Hooks.ManageHelpers (composeOne, isFullscreen, isDialog
+                                  , doFullFloat, doCenterFloat, (-?>))
 
--- TODO: would still like fullscreen flash vids to not crop and leave xmobar drawn
--- TODO: remove the red border when doing fullscreen? tried adding 'smartBorders' to the layoutHook but that didn't work
--- TODO: hook in TopicSpaces, start specific apps on specific workspaces
+{- Layout related stuff
+--------------------------------------------------}
+import XMonad.Layout.LayoutHints (layoutHints)
+import XMonad.Layout.NoBorders (noBorders, smartBorders)
+import XMonad.Layout.ResizableTile (ResizableTall(..))
+import XMonad.Layout.Spacing (spacing)
+import XMonad.Layout.Tabbed (Theme(..), tabbed, shrinkText)
+import XMonad.Layout.ToggleLayouts (toggleLayouts)
+
+{- Prompt
+--------------------------------------------------}
+import XMonad.Prompt (XPConfig(..))
+import XMonad.Prompt.AppLauncher (launchApp)
+
+{- Utils
+---------------------------------------------------}
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Scratchpad (scratchpadSpawnActionTerminal)
+
+
+import System.IO (Handle, hPutStrLn)
+import System.Environment (setEnv)
+import Graphics.X11.ExtraTypes.XF86 (xF86XK_MonBrightnessUp
+                                    , xF86XK_MonBrightnessDown)
 
 main :: IO ()
 main = do
   xmproc <- spawnPipe "/usr/local/bin/xmobar ~/.xmonad/xmobarrc"
-  xmonad $ def {
-      modMask = mod4Mask
-    , terminal = "urxvt"
+  setEnv "BROWSER" "conkeror"
+  xmonad $ def
+    { modMask = mod4Mask
+    , terminal = "termite"
     , layoutHook = myLayout
     , manageHook = myManageHook
     , workspaces = myWorkspaces
@@ -43,9 +59,8 @@ main = do
 
 myLogHook :: Handle -> X ()
 myLogHook h = do
-  copies <- wsContainingCopies
-  let check ws | ws `elem` copies = pad . xmobarColor "red" "black" $ ws
-               | otherwise = pad ws
+  let check ws | ws == "NSP" = mempty
+               | otherwise = ws
   dynamicLogWithPP myPP {ppHidden = check, ppOutput = hPutStrLn h}
 
 myManageHook :: ManageHook
@@ -58,9 +73,8 @@ myManageHook = composeOne [ isFullscreen -?> doFullFloat
                <+> manageDocks
 
 -- | Workspaces and their names.
--- Super-{1-9} and Super-Shift-{1-9} is
--- used to jump to the workspace with the given
--- index(+1) in the list.
+-- Super-{1-9} and Super-Shift-{1-9} is used to jump to the workspace
+-- with the given index(+1) in the list.
 myWorkspaces :: [String]
 myWorkspaces = [ "www"
                , "emacs"
@@ -78,23 +92,24 @@ myWorkspaces = [ "www"
 -- Defines additionally Super-0 and Super-Shift-0 for
 -- the "messenger" workspace
 myAdditionalKeys :: [((KeyMask, KeySym), X ())]
-myAdditionalKeys = [ ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +20")
-                   , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -20")
-                   , ((mod4Mask, xK_0), windows $ W.greedyView "messenger")
-                   , ((mod4Mask .|. shiftMask, xK_0), windows $ W.shift "messenger")
-                   , ((mod4Mask, xK_Down), scratchpadSpawnActionTerminal "urxvt")
-                   , ((mod4Mask, xK_v ), windows copyToAll) -- @@ Make focused window always visible
-                   , ((mod4Mask .|. shiftMask, xK_v ),  killAllOtherCopies) -- @@ Toggle window state back
-                   , ((mod4Mask, xK_g), promptSearch def google)
-                   , ((mod4Mask, xK_e), spawn "/home/jassob/.local/bin/startemacs")
-                   ]
+myAdditionalKeys =
+  [ ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +20")
+  , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -20")
+  , ((mod4Mask, xK_0), windows $ W.greedyView "messenger")
+  , ((mod4Mask .|. shiftMask, xK_0), windows $ W.shift "messenger")
+  , ((mod4Mask, xK_Down), scratchpadSpawnActionTerminal "termite")
+  , ((mod4Mask, xK_v ), windows copyToAll)
+  , ((mod4Mask .|. shiftMask, xK_v ),  killAllOtherCopies)
+  , ((mod4Mask, xK_g), promptSearch mySP{historySize=0} duckduckgo)
+  , ((mod4Mask, xK_e), spawn "/home/jassob/.local/bin/startemacs")
+  , ((mod4Mask, xK_b), launchApp mySP "conkeror")
+  ]
 
 myLayout = avoidStruts $ toggleLayouts (noBorders Full)
   (smartBorders (tiled
-                ||| Full
-                ||| mosaic 2 [3,2]
-                ||| spacing 5 (Mirror tiled)
-                ||| layoutHints (tabbed shrinkText myTab)))
+                 ||| Full
+                 ||| spacing 5 (Mirror tiled)
+                 ||| layoutHints (tabbed shrinkText myTab)))
   where
     tiled   = layoutHints $ ResizableTall nmaster delta ratio []
     nmaster = 1
@@ -103,59 +118,24 @@ myLayout = avoidStruts $ toggleLayouts (noBorders Full)
 
 -- * Themes
 
--- decoration theme
-myDeco :: Theme
-myDeco = def
-    { activeColor         = "orange"
-    , inactiveColor       = "#222222"
-    , urgentColor         = "yellow"
-    , activeBorderColor   = "orange"
-    , inactiveBorderColor = "#222222"
-    , urgentBorderColor   = "yellow"
-    , activeTextColor     = "orange"
-    , inactiveTextColor   = "#222222"
-    , urgentTextColor     = "yellow"
-    , decoHeight          = 10 }
-
 -- tab theme
 myTab :: Theme
-myTab = def
-    { activeColor         = "black"
-    , inactiveColor       = "black"
-    , urgentColor         = "yellow"
-    , activeBorderColor   = "orange"
-    , inactiveBorderColor = "#222222"
-    , urgentBorderColor   = "black"
-    , activeTextColor     = "orange"
-    , inactiveTextColor   = "#8e8e8e"
-    , urgentTextColor     = "yellow" }
+myTab = def { fontName   = "xft:inconsolata:size=12"
+            , decoHeight = 30 }
 
 -- shell prompt theme
 mySP :: XPConfig
-mySP = def
-    { bgColor           = "black"
-    , fgColor           = "white"
-    , bgHLight          = "gray"
-    , fgHLight          = "black"
-    , borderColor       = "orange"
-    , promptBorderWidth = 1
-    , position          = Bottom
-    , height            = 20
-    --, autoComplete      = Just 1000
-    , historySize       = 1000 }
+mySP = def { font   = "xft:inconsolata:size=12"
+           , height = 40 }
 
 myPP :: PP
 myPP = def { ppCurrent = xmobarColor "yellow" "" . wrap "[" "]"
            , ppVisible = wrap "(" ")"
            , ppTitle   = const ""
-           , ppHidden  = hideScratchpad
            , ppUrgent  = xmobarColor "red" "yellow"
            , ppLayout  = formatLayout
            }
-  where hideScratchpad "NSP" = ""
-        hideScratchpad x     = x
-
-        formatLayout x = case x of
+  where formatLayout x = case x of
           "Hinted ResizableTall"                  -> "[|]"
           "Spacing 5 Mirror Hinted ResizableTall" -> "[-]"
           "Hinted Tabbed Simplest"                -> "[T]"
