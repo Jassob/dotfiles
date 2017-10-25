55acad61b58d3f4f474bfa6035f4aa24fa19aa3f
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2017-10-26 01:48:17 +0200

    Updated the nix stuff.

--- xmonad/shell.nix
+++ xmonad/shell.nix
@@ -1,4 +1,43 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc821" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./default.nix {
-  backlight = nixpkgs.xorg.xbacklight;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, array, base, bytestring, containers
+      , data-default, data-default-class
+      , data-default-instances-containers, data-default-instances-dlist
+      , data-default-instances-old-locale, deepseq, directory, dlist
+      , extensible-exceptions, filepath, ghc-prim, integer-gmp, mtl
+      , old-locale, old-time, process, random, rts, setlocale, stdenv
+      , time, transformers, unix, utf8-string, X11, X11-xft, xmonad
+      , xmonad-contrib
+      }:
+      mkDerivation {
+        pname = "xmonad-config";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          array base bytestring containers data-default data-default-class
+          data-default-instances-containers data-default-instances-dlist
+          data-default-instances-old-locale deepseq directory dlist
+          extensible-exceptions filepath ghc-prim integer-gmp mtl old-locale
+          old-time process random rts setlocale time transformers unix
+          utf8-string X11 X11-xft xmonad xmonad-contrib
+        ];
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
