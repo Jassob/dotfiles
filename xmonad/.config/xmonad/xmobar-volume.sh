af6d1bc82482379833418aee3c4de050f172eb48
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-04-03 16:40:33 +0200

    Fix stupid old bug.

--- xmonad/.config/xmonad/xmobar-volume.sh
+++ xmonad/.config/xmonad/xmobar-volume.sh
@@ -5,7 +5,7 @@ VOL=$(pamixer --get-volume)
 MUTE=$(pamixer --get-mute)
 
 # Add the muted line if the Master device is off (muted)
-if [[ $LINE =~ "true" ]] ; then
+if [[ $MUTE =~ "true" ]] ; then
     echo "muted (${VOL}%)"
 else
     echo "${VOL}%"
