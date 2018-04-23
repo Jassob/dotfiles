0a01e5f975ae5c8ef042dfb1787182e86c8d4c05
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-04-23 12:02:13 +0200

    Add shortcuts to nix-env stuff.

--- zsh/.zsh.d/lib/nix.zsh
+++ zsh/.zsh.d/lib/nix.zsh
@@ -12,9 +12,30 @@ haskell-env-hoogle () {
 	env-type "haskellEnvHoogle" "$@"
 }
 
-nix-search() {
-	echo "Searching..."
-	nix-env -qaP | grep -i "$1"
+nix-search () {
+    echo "Searching for \"$1\" in default attribute set..."
+    nix-env -qaP | grep -i "$1"
+}
+
+nix-search-haskell-packages () {
+    echo "Searching for \"$1\" in attribute haskellPackages..."
+    list-haskell-packages | grep -i "$1"
+}
+
+list-haskell-packages () {
+    nix-env -qaP -A haskellPackages
+}
+
+list-nixpkgs () {
+    nix-env -qaP
+}
+
+list-python-packages () {
+    nix-env -qaP -A python3Packages
+}
+
+list-python2-packages () {
+    nix-env -qaP -A pythonPackages
 }
 
 # Link ~/.nix-defexpr -> ~/nixpkgs
