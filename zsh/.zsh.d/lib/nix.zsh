c1b9a10ebadf32e9846ae2d07ddfee5d19b04959
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-06-03 14:08:38 +0200

    Indentation in nix.zsh

--- zsh/.zsh.d/lib/nix.zsh
+++ zsh/.zsh.d/lib/nix.zsh
@@ -1,15 +1,15 @@
 env-type () {
-	envtype="$1"
-	shift
-	nix-shell -Q -p $envtype "$@"
+    envtype="$1"
+    shift
+    nix-shell -Q -p $envtype "$@"
 }
 
 haskell-env () {
-	env-type "haskellEnv" "$@"
+    env-type "haskellEnv" "$@"
 }
 
 haskell-env-hoogle () {
-	env-type "haskellEnvHoogle" "$@"
+    env-type "haskellEnvHoogle" "$@"
 }
 
 nix-search () {
