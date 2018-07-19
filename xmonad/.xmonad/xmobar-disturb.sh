86c1e7aa213623254d3491a9bab2b0cce235f7b8
Jacob Jonsson <Jassob@users.noreply.github.com>
2018-07-19 08:24:37 +0200

    Make xmobar-disturb POSIX shell compliant

--- xmonad/.xmonad/xmobar-disturb.sh
+++ xmonad/.xmonad/xmobar-disturb.sh
@@ -1,17 +1,17 @@
-#! env bash
+#!/usr/bin/env sh -eo
 
 ENV_DIR="${HOME}/.cache"
 ENV_FILE="${ENV_DIR}/disturb"
 DO_NOT_DISTURB=$(cat "${ENV_FILE}")
 
-if [[ ! -d ${ENV_FILE} ]]; then
+if [ ! -d ${ENV_FILE} ]; then
     mkdir -p "${ENV_DIR}"
     echo "false" > "${ENV_FILE}"
 fi
 
-if [[ ${DO_NOT_DISTURB} == "true" ]]; then
+if [ ${DO_NOT_DISTURB} = "true" ]; then
 	echo ""
-elif [[ ${DO_NOT_DISTURB} == "false" ]]; then
+elif [ ${DO_NOT_DISTURB} = "false" ]; then
 	echo ""
 else
 	echo "error: DO_NOT_DISTURB is not set to either true or false"
