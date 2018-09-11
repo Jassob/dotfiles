17a8740f3b7535b643387434d7127a6b7bed9098
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-09-11 14:10:59 +0200

    Updated trayer command

--- xmonad/.xmonad/xmobar-trayer.sh
+++ xmonad/.xmonad/xmobar-trayer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-trayer --edge top --align right --width 5 --transparent true --alpha 255 -l
+trayer --edge top --align right --width 5 --transparent true --alpha 255 -l --monitor primary
