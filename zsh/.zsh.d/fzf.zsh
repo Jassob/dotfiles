badfaa8e12231d2c0e384c1e67b8ebd5943fa0f5
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-10-25 15:09:33 +0200

    zsh: Inline non-Nix FZF in fzf.zsh

--- zsh/.zsh.d/fzf.zsh
+++ zsh/.zsh.d/fzf.zsh
@@ -1,11 +1,28 @@
 # Setup fzf
 # ---------
-FZF_PATH=$(whereis fzf | awk '{ print $2 }' | xargs dirname | xargs dirname)
+if [[ -e ~/.fzf ]]; then
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */home/jassob/.fzf/bin* ]]; then
+	export PATH="$PATH:/home/jassob/.fzf/bin"
+    fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$FZF_PATH/share/fzf/completion.zsh" 2> /dev/null
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/home/jassob/.fzf/shell/completion.zsh" 2> /dev/null
 
-# Key bindings
-# ------------
-source "$FZF_PATH/share/fzf/key-bindings.zsh"
+    # Key bindings
+    # ------------
+    source "/home/jassob/.fzf/shell/key-bindings.zsh"
+else
+    # Find Nix path to FZF
+    FZF_PATH=$(whereis fzf | awk '{ print $2 }' | xargs dirname | xargs dirname)
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "$FZF_PATH/share/fzf/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "$FZF_PATH/share/fzf/key-bindings.zsh"
+fi
