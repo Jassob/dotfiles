6b9e14e9739e5684fa781cc6c43ab001fabfe2f4
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2019-02-19 22:48:19 +0100

    Only put static executables in prompt scripts

--- jassob/prompt-scripts/shell.nix
+++ jassob/prompt-scripts/shell.nix
@@ -28,7 +28,7 @@ let
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  drv = variant (haskellPackages.callPackage f {});
+  drv = variant (pkgs.haskell.lib.justStaticExecutables (haskellPackages.callPackage f {}));
 
 in
 
