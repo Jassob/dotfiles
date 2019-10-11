69e81e09caf48dbe8743c3369f42bfef84dc9371
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2019-10-11 17:26:04 +0200

    fix(xmonad): Use more recommended derivation setup
    This is a more recommended setup for Nix derivations, as we can more
    or less copy both shell.nix and release.nix to new projects and then
    simply regenerate default.nix with cabal2nix.

--- xmonad/src/shell.nix
+++ xmonad/src/shell.nix
@@ -1,43 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+{ nixpkgs ? import <nixpkgs> {} }:
+  let
+    inherit (nixpkgs) pkgs;
+    inherit (pkgs) haskellPackages;
 
-let
-
-  inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, array, base, bytestring, containers
-      , data-default, data-default-class
-      , data-default-instances-containers, data-default-instances-dlist
-      , data-default-instances-old-locale, deepseq, directory, dlist
-      , extensible-exceptions, filepath, ghc-prim, integer-gmp, mtl
-      , old-locale, old-time, process, random, rts, setlocale, stdenv
-      , time, transformers, unix, utf8-string, X11, X11-xft, xmonad
-      , xmonad-contrib
-      }:
-      mkDerivation {
-        pname = "xmonad-config";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [
-          array base bytestring containers data-default data-default-class
-          data-default-instances-containers data-default-instances-dlist
-          data-default-instances-old-locale deepseq directory dlist
-          extensible-exceptions filepath ghc-prim integer-gmp mtl old-locale
-          old-time process random rts setlocale time transformers unix
-          utf8-string X11 X11-xft xmonad xmonad-contrib
-        ];
-        license = stdenv.lib.licenses.bsd3;
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+    project = import ./release.nix;
+  in
+pkgs.stdenv.mkDerivation {
+  name = "shell";
+  buildInputs = project.env.nativeBuildInputs ++ [
+    haskellPackages.cabal-install
+  ];
+}
