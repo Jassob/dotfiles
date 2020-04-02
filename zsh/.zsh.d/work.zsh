dcab322a2310971da9626b1ab26f212f98ed8165
Jacob Jonsson <Jassob@users.noreply.github.com>
2020-04-02 15:22:25 +0200

    feat(zsh): Add some nifty scripts to make life easier
    When merging a branch that branch is often removed on the remote, but
    not locally. Therefore this function removes all branches that are
    not present on a remote.

--- zsh/.zsh.d/work.zsh
+++ zsh/.zsh.d/work.zsh
@@ -16,3 +16,29 @@ function gt() {
     done
     echo "$FAILED tests out of $NUM failed"
 }
+
+function git-cleanup-branches() {
+    local REMOVED=0
+    for b in $(git branch | grep -v \* | grep -v master); do
+	git branch -r | grep ${b} > /dev/null || REMOVED=$((REMOVED + 1)); git branch -D ${b}
+    done
+    echo "Cleaned up $REMOVED branches"
+}
+
+# Select Go version and update environment to use
+#
+# Usage: use-go 1.13      -- Uses Go version 1.13
+#        use-go 1.14      -- Uses Go version 1.14
+#        use-go           -- Uses default Go version (currently 1.14)
+function use-go() {
+    GO_VERSION=${1:-1.14} # Use Go 1.14 if no argument is given
+    if [[ -z $OLDPATH ]]; then
+	export OLDPATH=$PATH
+    fi
+    export GOROOT="$HOME/.local/share/devtools/go/${GO_VERSION}/go"
+    export GOPATH=$HOME/go
+    export PATH=$GOPATH/bin:$OLDPATH
+    export PATH=$GOROOT/bin:$OLDPATH
+    export GOPRIVATE=github.com/einride/*
+    echo "Go environment set up for Go $GO_VERSION"
+}
