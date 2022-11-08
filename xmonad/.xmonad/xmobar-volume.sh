a48c827de74bb8cd39d65dcbfcc472fb2536a9c3
Jacob Jonsson <Jassob@users.noreply.github.com>
2022-11-08 22:25:36 +0100

    chore(xmonad): script updates

--- xmonad/.xmonad/xmobar-volume.sh
+++ xmonad/.xmonad/xmobar-volume.sh
@@ -5,13 +5,13 @@ VOL=$(pulsemixer --get-volume | awk '{ print $1 }')
 MUTE=$(pulsemixer --get-mute)
 
 # Add the muted line if the Master device is off (muted)
-if [ $MUTE = "1" ] ; then
+if [ "${MUTE}" = "1" ]; then
     ICON="婢"
-elif [ "$VOL" -le 15 ]; then
+elif [ "${VOL}" -le 15 ]; then
     ICON="奄"
-elif [ "$VOL" -le 50 ]; then
+elif [ "${VOL}" -le 50 ]; then
     ICON="奔"
-elif [ "$VOL" -le 75 ]; then
+elif [ "${VOL}" -le 75 ]; then
     ICON="墳"
 else
     ICON=""
