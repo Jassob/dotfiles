f0bd76633ea7125425f6140a9a1fbf7b42f80bc8
Jacob Jonsson <Jassob@users.noreply.github.com>
2022-12-31 14:17:55 +0100

    chore(xmonad/xmobar): format file with brittany

--- xmonad/src/xmobar/Main.hs
+++ xmonad/src/xmobar/Main.hs
@@ -1,19 +1,19 @@
-import Xmobar
+import           Xmobar
 
 config :: Config
 config = defaultConfig
-  { font            = "Iosevka Nerd Font Mono 16"
-  , alpha           = 0x50
-  , bgColor         = "black"
-  , fgColor         = "#f8f8f8"
-  , position        = TopSize L 95 29
+  { font        = "Iosevka Nerd Font Mono 16"
+  , alpha       = 0x50
+  , bgColor     = "black"
+  , fgColor     = "#f8f8f8"
+  , position    = TopSize L 95 29
 
   -- general behavior
   , allDesktops = True    -- show on all desktops
-  , sepChar = "%"
-  , alignSep = "}{"
-  , template =
-      "%XMonadLog%\
+  , sepChar     = "%"
+  , alignSep    = "}{"
+  , template    =
+    "%XMonadLog%\
       \ } \
       \<action=`$HOME/.local/bin/startemacs -e '(org-agenda-list)'` button=1>\
       \\61555 %date%\
@@ -33,54 +33,79 @@ config = defaultConfig
       \| %vol%\
       \</action> \
       \| %battery% "
-  , commands =
-      [ Run XMonadLog
-
-      , Run $ Date "<action=`$BROWSER https://calendar.google.com`>%b %d, %H:%M</action>" "date" 10
-
-      , Run $ Battery
-        [ "--template" , "<acstatus> <left>"
-        , "-L", "10"
-        , "-H", "75"
-        , "--", "-P" -- Add percentage
-        , "-L", "25"
-        , "-H", "75"
-        , "-h", "green"
-        , "-m", "darkorange"
-        , "-l", "darkred"
-        , "-i", "\62840"
-        , "-O", "\62851"
-        , "-o", ""
-        , "--highs"  , "\62849"
-        , "--mediums", "\62845"
-        , "--lows"   , "\62850"
-        , "-A", "10"
-        , "-a", "notify-send -u critical 'Battery level low, connect charger!'"
-        ] 5
-
-      , Run $ Mpris2 "spotify"
-        ["-t", "<action=`playerctl play-pause`><artist> - <title></action>"
-         , "-M", "20"
-         , "-x", ""
-         ] 10
-
-      , Run $ ComX "sh" [ "-c"
-                      , "mu find flag:unread and maildir:/INBOX and not flag:trashed | wc -l"
-                      ] "N/A" "inbox" 10
-      , Run $ ComX "sh" [ "-c"
-                      , "mu find flag:flagged and not flag:trashed | uniq | wc -l"
-                      ] "N/A" "starred" 10
-
-      , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-volume.sh"] "vol" 10
-
-      , Run $ Com "sh" ["-c", "checkupdates | wc -l"] "checkupdates" 3600
-
-      , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-disturb.sh"] "disturb" 10
-
-      , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-awake.sh"] "awake" 10
-
-      , Run $ Memory [ "-t", "Mem: <usedratio>%" ] 10
+  , commands    =
+    [ Run XMonadLog
+    , Run $ Date
+      "<action=`$BROWSER https://calendar.google.com`>%b %d, %H:%M</action>"
+      "date"
+      10
+    , Run $ Battery
+      [ "--template"
+      , "<acstatus> <left>"
+      , "-L"
+      , "10"
+      , "-H"
+      , "75"
+      , "--"
+      , "-P" -- Add percentage
+      , "-L"
+      , "25"
+      , "-H"
+      , "75"
+      , "-h"
+      , "green"
+      , "-m"
+      , "darkorange"
+      , "-l"
+      , "darkred"
+      , "-i"
+      , "\62840"
+      , "-O"
+      , "\62851"
+      , "-o"
+      , ""
+      , "--highs"
+      , "\62849"
+      , "--mediums"
+      , "\62845"
+      , "--lows"
+      , "\62850"
+      , "-A"
+      , "10"
+      , "-a"
+      , "notify-send -u critical 'Battery level low, connect charger!'"
       ]
+      5
+    , Run $ Mpris2
+      "spotify"
+      [ "-t"
+      , "<action=`playerctl play-pause`><artist> - <title></action>"
+      , "-M"
+      , "20"
+      , "-x"
+      , ""
+      ]
+      10
+    , Run $ ComX
+      "sh"
+      [ "-c"
+      , "mu find flag:unread and maildir:/INBOX and not flag:trashed | wc -l"
+      ]
+      "N/A"
+      "inbox"
+      10
+    , Run $ ComX
+      "sh"
+      ["-c", "mu find flag:flagged and not flag:trashed | uniq | wc -l"]
+      "N/A"
+      "starred"
+      10
+    , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-volume.sh"] "vol" 10
+    , Run $ Com "sh" ["-c", "checkupdates | wc -l"] "checkupdates" 3600
+    , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-disturb.sh"] "disturb" 10
+    , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-awake.sh"] "awake" 10
+    , Run $ Memory ["-t", "Mem: <usedratio>%"] 10
+    ]
   }
 
 main :: IO ()
