f79ce12d78166b4174b201f3b6cc1e342eb36b25
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2023-08-16 15:49:32 +0200

    chore(emacs): load custom file on boot
    Otherwise settings will not be read, only stored :face-palm:

--- xmonad/src/xmobar/Main.hs
+++ xmonad/src/xmobar/Main.hs
@@ -15,77 +15,54 @@ config = defaultConfig
   , template    = myTemplate
   , commands    =
     [ Run XMonadLog
+
     , Run $ Date
-      "<action=`$BROWSER https://calendar.google.com`>%b %d, %H:%M</action>"
-      "date"
-      10
+      "<action=`$BROWSER https://calendar.google.com`>%b %d, %H:%M</action>"  "date" 10
+
     , Run $ Battery
       [ "--template"
       , "<acstatus> <left>"
-      , "-L"
-      , "10"
-      , "-H"
-      , "75"
-      , "--"
-      , "-P" -- Add percentage
-      , "-L"
-      , "25"
-      , "-H"
-      , "75"
-      , "-h"
-      , "green"
-      , "-m"
-      , "darkorange"
-      , "-l"
-      , "darkred"
-      , "-i"
-      , "\62840"
-      , "-O"
-      , "\62851"
-      , "-o"
-      , ""
-      , "--highs"
-      , "\62849"
-      , "--mediums"
-      , "\62845"
-      , "--lows"
-      , "\62850"
-      , "-A"
-      , "10"
-      , "-a"
-      , "notify-send -u critical 'Battery level low, connect charger!'"
+      , "-L", "10"
+      , "-H", "75"
+      , "--", "-P" -- Add percentage
+      , "-L", "25"
+      , "-H", "75"
+      , "-h", "green"
+      , "-m", "darkorange"
+      , "-l", "darkred"
+      , "-i", "\62840"
+      , "-O", "\62851"
+      , "-o", ""
+      , "--highs"  , "\62849"
+      , "--mediums", "\62845"
+      , "--lows"   , "\62850"
+      , "-A", "10"
+      , "-a", "notify-send -u critical 'Battery level low, connect charger!'"
+      ] 5
+
+      , Run $ Mpris2 "spotify"
+        ["-t", "<action=`playerctl play-pause`><artist> - <title></action>"
+         , "-M", "20"
+         , "-x", ""
+         ] 10
+
+      , Run $ ComX "sh" [ "-c"
+                      , "mu find flag:unread and maildir:/INBOX and not flag:trashed | wc -l"
+                      ] "N/A" "inbox" 10
+      , Run $ ComX "sh" [ "-c"
+                      , "mu find flag:flagged and not flag:trashed | uniq | wc -l"
+                      ] "N/A" "starred" 10
+
+      , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-volume.sh"] "vol" 10
+
+      , Run $ Com "sh" ["-c", "checkupdates | wc -l"] "checkupdates" 3600
+
+      , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-disturb.sh"] "disturb" 10
+
+      , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-awake.sh"] "awake" 10
+
+      , Run $ Memory [ "-t", "Mem: <usedratio>%" ] 10
       ]
-      5
-    , Run $ Mpris2
-      "spotify"
-      [ "-t"
-      , "<action=`playerctl play-pause`><artist> - <title></action>"
-      , "-M"
-      , "20"
-      , "-x"
-      , ""
-      ]
-      10
-    , Run $ ComX
-      "sh"
-      [ "-c"
-      , "mu find flag:unread and maildir:/INBOX and not flag:trashed | wc -l"
-      ]
-      "N/A"
-      "inbox"
-      10
-    , Run $ ComX
-      "sh"
-      ["-c", "mu find flag:flagged and not flag:trashed | uniq | wc -l"]
-      "N/A"
-      "starred"
-      10
-    , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-volume.sh"] "vol" 10
-    , Run $ Com "sh" ["-c", "checkupdates | wc -l"] "checkupdates" 3600
-    , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-disturb.sh"] "disturb" 10
-    , Run $ Com "sh" ["/home/jassob/.xmonad/xmobar-awake.sh"] "awake" 10
-    , Run $ Memory ["-t", "Mem: <usedratio>%"] 10
-    ]
   }
 
 myTemplate :: String
