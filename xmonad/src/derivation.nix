03cc6373b9b8d4873196fbf63dbd9c222c514653
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2026-07-05 12:22:28 +0200

    chore(xmonad): bump dependencies for newer Haskell version
    This commit updates the build instructions and cabal definitions to
    make it buildable on Haskell 9.10.3.

--- xmonad/src/derivation.nix
+++ xmonad/src/derivation.nix
@@ -1,6 +1,4 @@
 { mkDerivation, array, base, bytestring, containers, data-default
-, data-default-class, data-default-instances-containers
-, data-default-instances-dlist, data-default-instances-old-locale
 , deepseq, directory, dlist, extensible-exceptions, filepath
 , ghc-prim, integer-gmp, mtl, old-locale, old-time, process, random
 , rts, setlocale, stdenv, time, transformers, unix, utf8-string
@@ -13,9 +11,7 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    array base bytestring containers data-default data-default-class
-    data-default-instances-containers data-default-instances-dlist
-    data-default-instances-old-locale deepseq directory dlist
+    array base bytestring containers data-default deepseq directory dlist
     extensible-exceptions filepath ghc-prim integer-gmp mtl old-locale
     old-time process random rts setlocale time transformers unix
     utf8-string X11 X11-xft xmonad xmonad-contrib xmobar
